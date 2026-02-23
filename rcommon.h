53836cef7e20ea4a70414c5f1996bd0eba371c4c
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-02-23 02:28:13 +0100

    Initial commit

---
+++ rcommon.h
@@ -0,0 +1,39 @@
+#ifndef __R_COMMON_H__
+#define __R_COMMON_H__ 
+
+#include <stdint.h>
+#include <ctype.h>
+
+
+typedef unsigned char   uint8_t;
+typedef unsigned short  uint16_t;
+typedef unsigned int    uint32_t;
+
+typedef uint8_t         r_uint8;
+typedef uint16_t        r_uint16;
+typedef uint32_t        r_uint32;
+typedef uint64_t        r_uint64;
+typedef int8_t          r_int8;
+typedef int16_t         r_in16;
+typedef int32_t         r_int32;
+typedef int64_t         r_int64;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif  /* __R_COMMON_H__ END */
